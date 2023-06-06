049c34440f1169002c61383eaa3ab546551e7b50
luckys-long <126213954+luckys-long@users.noreply.github.com>
2023-06-06 18:10:52 +0800

    Update index.cjs
    t调整

--- config/index.cjs
+++ config/index.cjs
@@ -43,9 +43,8 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2018-09-31' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+       { keyword: 'love_day', date: '2019-04-29' },
+       
       ],
     },
   ],
