4754eaba3ebbdba10d43bb0e1d7f83dc3910dace
luckys-long <126213954+luckys-long@users.noreply.github.com>
2023-06-07 09:59:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'owHmb6FHeEpxeWGiJ3HqnPtp8llI',
+      id: 'owHmb6NHHQ1bkGQvGOWWm9e9pUBI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'VLoAKTdOJ0bHSQPKsaV_WwXgqPn-O1_6F0G3Dwi8naI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
