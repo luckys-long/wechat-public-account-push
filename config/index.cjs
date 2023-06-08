d90687fe22ffb2d39eb5f6b8a63c231689b49fe6
luckys-long <126213954+luckys-long@users.noreply.github.com>
2023-06-08 10:36:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,6 +47,35 @@ const USER_CONFIG = {
        
       ],
     },
+     {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'owHmb6NHHQ1bkGQvGOWWm9e9pUBI',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '-hBoh2ejy68hBil6dmooierf2QdLl2PVmJczr9bA4uY',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '10-06',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '08-24',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '1996', date: '10-06',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2019', date: '04-29',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+       { keyword: 'love_day', date: '2019-04-29' },
+       
+      ],
+    },
   ],
 
 
